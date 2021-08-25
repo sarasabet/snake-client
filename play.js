071bcd748052f3f4c42e103a29a80ff301aa66f2
Sara Sabet <sara.ilbeigi@gmail.com>
2021-08-25 19:37:03 +0000

    processexit UI

--- play.js
+++ play.js
@@ -4,5 +4,23 @@ const {connect} = require('./client')
 let conn = connect();
 
 console.log("Connecting ...");
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  return stdin;
+};
+// to process exit , get standard input from client and if it is Ctrl+ c process exit
+const handleUserInput = function () {
+  stdin.on('data', (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  });
+};
+
 connect();
 
